d7c161ae9dca6de3f7bf8f1103b13a06e155659a
felipexlr50 <felipexlr50@hotmail.com>
2016-08-28 17:31:39 -0300

    Adicionado API token

--- salvarImagem.php
+++ salvarImagem.php
@@ -1,26 +1,44 @@
 <?php
 
+include "APIKEY_VERIFY.php";
+
+//$mysqli = mysqli_connect("localhost", "root", "","aedesmap");
 $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_aedes");
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-	// Get data
-	//$imageBlob = mysqli_real_escape_string($mysqli,$_POST['imageBlob']);
-	$latitude = floatval($_POST['latitude']);
-	$longitude = floatval($_POST['longitude']);
-	
-	// Insert data into data base
-	$sql = "INSERT INTO imagem ( latitude, longitude) 
-			VALUES ($latitude, $longitude);";
-	$qur = $mysqli->query($sql);
-	if($qur){
-		$json = array("status" => 1, "msg" => "OK");
-	}else{
-		$json = array("status" => 0, "msg" => "Error");
+
+	$password = "batata";
+	$APIKEY = $_SERVER['HTTP_X_API_KEY'];
+	list($token,$timestamp) = explode("-",$APIKEY);
+	$hash = hash("sha256",$password . $timestamp);
+
+	if($hash==$token && testTimeStamp($timestamp,600)){
+
+			// Get data
+			//$imageBlob = mysqli_real_escape_string($mysqli,$_POST['imageBlob']);
+			$latitude = floatval($_POST['latitude']);
+			$longitude = floatval($_POST['longitude']);
+			
+			// Insert data into data base
+			$sql = "INSERT INTO imagem ( latitude, longitude) 
+					VALUES ($latitude, $longitude);";
+			$qur = $mysqli->query($sql);
+			if($qur){
+				$json = array("status" => 1, "msg" => "OK");
+			}else{
+				$json = array("status" => 0, "msg" => "Error");
+			}
+
+	}else {
+		$json = array("status" => 0, "msg" => "Api key invalided!","recived"=>$APIKEY,
+		"generated" => $hash);
 	}
+	
+	
 }
 
 else{
-	$json = array("status" => 0, "msg" => "Request method not accepted");
+	$json = array("status" => 0, "msg" => "Request method not accepted!");
 }
 
 mysqli_close($mysqli);
