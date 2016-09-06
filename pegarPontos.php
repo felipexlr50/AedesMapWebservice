2a2f2852482fc7a30334af3d69ed28edfd2cab5b
felipexlr50 <felipexlr50@hotmail.com>
2016-09-06 17:08:20 -0400

    teste salvar

--- pegarPontos.php
+++ pegarPontos.php
@@ -9,11 +9,12 @@ $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_
     $resultArray =array();
     
     while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+        $imageString = $row['imgageBlob']
         $latitude = $row['latitude'];
         $longitude = $row['longitude'];
         $dataInserida = $row['dataInserida'];
        // $resultArray[] = array("{latitude : $latitude","longitude : $longitude","datahora : $dataInserida}" );
-       $resultArray[] = array('latitude'=> $latitude,'longitude'=> $longitude,'datahora' => $dataInserida ); 
+       $resultArray[] = array('latitude'=> $latitude,'longitude'=> $longitude,'datahora' => $dataInserida, 'imgString' => $imageString ); 
     }
     
     $json = array("status" => 1, "info" => $resultArray);
