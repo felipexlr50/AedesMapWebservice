deca5e7960e148b7b28175aa84f10032eaaf1b52
felipexlr50 <felipexlr50@hotmail.com>
2016-09-13 15:10:56 -0400

    arrumando erro

--- pegarPontos.php
+++ pegarPontos.php
@@ -3,13 +3,13 @@
 $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_aedes");
  
     
-    $sql = "select latitude,longitude, dataInserida from imagem ";
+    $sql = "select imageBlob, latitude,longitude, dataInserida from imagem ";
     $result = $mysqli->query($sql); //mysqli_query($mysqli,$sql);
     // printf("Error: %s\n", mysqli_error($mysqli));
     $resultArray =array();
     
     while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-        $imageString = $row['imgageBlob'];
+        $imageString = $row['imageBlob'];
         $latitude = $row['latitude'];
         $longitude = $row['longitude'];
         $dataInserida = $row['dataInserida'];
