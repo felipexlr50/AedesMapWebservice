ea4159bb957332e566de611749ba09d790f8a59a
felipexlr50 <felipexlr50@hotmail.com>
2016-10-15 21:21:20 -0400

    adding toPablo.php

--- toPablo.php
+++ toPablo.php
@@ -2,7 +2,7 @@
 if($_SERVER['REQUEST_METHOD'] == "POST"){
   
   $url = $_POST['url'];
-  ini_set("allow_url_fopen", 1);
+  
   $json = file_get_contents(url);
   header('Access-Control-Allow-Origin: *');
    header('Content-type: application/json');
