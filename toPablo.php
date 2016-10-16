6d6e6d37750384fbb3b3191b61943559a140e9ac
felipexlr50 <felipexlr50@hotmail.com>
2016-10-15 22:07:22 -0400

    adding toPablo.php

--- toPablo.php
+++ toPablo.php
@@ -1,13 +1,26 @@
 <?php
+
+function curl_get_contents($url)
+{
+  $ch = curl_init($url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+  $data = curl_exec($ch);
+  curl_close($ch);
+  return $data;
+}
+
 if($_SERVER['REQUEST_METHOD'] == "POST"){
   
   $url = $_POST['url'];
   
- // $json = file_get_contents(url);
-  header('Access-Control-Allow-Origin: *');
-   header('Content-type: application/json');
+ $json = curl_get_contents($url);
+	 header('Access-Control-Allow-Origin: *');
+     header('Content-type: application/json');
   
-  echo $url;
+  echo $json;
 }
 else{
   echo "essa url não funciona!";
