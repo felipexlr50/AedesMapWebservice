4d480062a3bef7512f7d3a67df396a51b7d12c86
felipexlr50 <felipexlr50@hotmail.com>
2016-10-15 21:25:14 -0400

    adding toPablo.php

--- toPablo.php
+++ toPablo.php
@@ -3,11 +3,11 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
   
   $url = $_POST['url'];
   
-  $json = file_get_contents(url);
+ // $json = file_get_contents(url);
   header('Access-Control-Allow-Origin: *');
    header('Content-type: application/json');
   
-  echo $json;
+  echo $url;
 }
 else{
   echo "essa url não funciona!";
