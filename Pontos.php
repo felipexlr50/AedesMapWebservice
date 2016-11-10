6a88fa68d74c3427e3b2ccc2d3216e1ebb72ed56
felipexlr50 <felipexlr50@hotmail.com>
2016-11-10 13:12:07 -0500

    adicionando novas apis

--- Pontos.php
+++ Pontos.php
@@ -1,9 +1,11 @@
 <?php
 include "APIKEY_VERIFY.php";
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+
 $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_aedes");
 
       
-       $sql = "select imageBlob, latitude,longitude, dataInserida from imagem ";
+       $sql = "select imageBlob, latitude,longitude, dataInserida from imagem WHERE status = 'T' ;";
        $result = $mysqli->query($sql);
        $resultArray =array();
     
@@ -11,18 +13,15 @@ $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_
             $latitude = $row['latitude'];
             $longitude = $row['longitude'];
             $dataInserida = $row['dataInserida'];
-            $imagemBlob = $row['imagemBlob'];
-            $resultArray[] = array('latitude'=> $latitude,'longitude'=> $longitude,'datahora' => $dataInserida, 'imagemBlob' => $imagemBlob ); 
+            $resultArray[] = array('latitude'=> $latitude,'longitude'=> $longitude,'datahora' => $dataInserida ); 
         }
     
         $json = array("status" => true, "data" => $resultArray);
  
        @mysqli_close($mysqli);
-
+}else{
+	$json = array("status" => false, "msg" => "Request method not accepted!");
+}
    header('Access-Control-Allow-Origin: *');
    header('Content-type: application/json');
    echo json_encode($json);
-
- 
-    
-   
---
+++ login.php
@@ -0,0 +1,48 @@
+<?php
+include "APIKEY_VERIFY.php";
+
+$mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_aedes");
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+  $password = "batata";
+	$APIKEY = $_POST['API_KEY'];
+	list($token,$timestamp) = explode("-",$APIKEY);
+	$hash = hash("sha256",$password . $timestamp);
+
+	if($hash==$token && testTimeStamp($timestamp,600)){
+    $email = $_POST['id_admin'];
+    $senhaClient = $_POST['senha'];
+    $sql = "SELECT DISTINCT * FROM admin WHERE email = '$email';";
+       $result = $mysqli->query($sql);
+       $error = $mysqli->error;
+    while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $senha = $row['senha'];
+            $id_admin = $row['id_admin'];
+            $nome = $row['Nome'];
+            
+    }
+    if($senha == $senhaClient){
+        $json = array("status" => true, "msg" => "Logado", "id" => $id_admin, "nome" => $nome );
+    }
+    else{
+		 	$json = array("status" => false, "msg" => "Senha ou login errados");
+		}
+  }
+  
+  else {
+		$json = array("status" => false, "msg" => "Api key invalided!");
+	}
+}
+
+else{
+	$json = array("status" => false, "msg" => "Request method not accepted!");
+}
+
+mysqli_close($mysqli);
+
+/* Output header */
+	header('Access-Control-Allow-Origin: *');
+	header('Content-type: application/json');
+	echo json_encode($json);
+
+?>
