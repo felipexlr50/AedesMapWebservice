80b35a22b2081ba5cc8f95d79b71b47c5bf513a0
felipexlr50 <felipexlr50@hotmail.com>
2016-11-12 12:44:53 -0500

    arrumando api

--- pegarPontos.php
+++ pegarPontos.php
@@ -11,9 +11,9 @@ $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_
             $latitude = $row['latitude'];
             $longitude = $row['longitude'];
             $dataInserida = $row['dataInserida'];
-            $imagemBlob = $row['imagemBlob'];
+            $imagemBlob = $row['imageBlob'];
             $imagem_id = $row['id'];
-            $resultArray[] = array( 'id' => $imagem_id, 'latitude'=> $latitude,'longitude'=> $longitude,'datahora' => $dataInserida, 'imagemBlob' => $imagemBlob ); 
+            $resultArray[] = array( 'id' => $imagem_id, 'latitude'=> $latitude,'longitude'=> $longitude,'datahora' => $dataInserida, 'imageBlob' => $imagemBlob ); 
         }
     
         $json = array("status" => true, "data" => $resultArray);
