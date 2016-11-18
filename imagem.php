baa27c2a773d73b94d3faf6ee357fe3b7a96f300
Felipe Machado Baptista <felipexlr50@hotmail.com>
2016-11-18 19:56:29 -0200

    Update imagem.php
    arrumando

--- imagem.php
+++ imagem.php
@@ -6,16 +6,22 @@ $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_
          
        $sql = "select * from imagem where status = 'F' and id = $id";
        $result = $mysqli->query($sql);
+       $error = $mysqli->error;
        $resultArray =array();
-    
+       if($result){ 
         while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
            $imagem_id = $row['id'];
            $imagemBlob = $row['imageBlob'];
-          
+         
+            
             $resultArray[] = array( 'id' => $imagem_id,'imageBlob' => $imagemBlob); 
         }
-    
-        $json = array("status" => true, "data" => $resultArray);
+          $json = array("status" => true, "data" => $resultArray);
+       }else{
+         $json = array("status" => false, "msg" => $error);
+       }
+
+       
  
        @mysqli_close($mysqli);
 
