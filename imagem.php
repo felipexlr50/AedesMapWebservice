3ef9d50b49776a9d4c69a3b97580021b83dc25db
Felipe Machado Baptista <felipexlr50@hotmail.com>
2016-11-18 20:03:45 -0200

    Update imagem.php
    arrumando

--- imagem.php
+++ imagem.php
@@ -2,8 +2,10 @@
 include "APIKEY_VERIFY.php";
 $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_aedes");
 
-       $id = (int) $_GET['id'];
-         
+      if($_SERVER['REQUEST_METHOD'] == "POST"){
+        
+       
+       $id = intval($_POST['id']); 
        $sql = "select * from imagem where status = 'F' and id = $id";
        $result = $mysqli->query($sql);
        $error = $mysqli->error;
@@ -12,14 +14,17 @@ $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_
         while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
            $imagem_id = $row['id'];
            $imagemBlob = $row['imageBlob'];
-         
-            
-            $resultArray[] = array( 'id' => $imagem_id,'imageBlob' => $imagemBlob); 
         }
-          $json = array("status" => true, "data" => $resultArray);
+          $json = array("status" => true, 'id' => $imagem_id,'imageBlob' => $imagemBlob);
        }else{
          $json = array("status" => false, "msg" => $error);
        }
+        
+      }else{
+	      $json = array("status" => false, "msg" => "Request method not accepted!");
+    }
+         
+      
 
        
  
