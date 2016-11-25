18c649a8cbfa8a20647c1fd7f8efd86360ace3e4
felipexlr50 <felipexlr50@hotmail.com>
2016-11-24 20:44:56 -0500

    teste

--- imagem.php
+++ imagem.php
@@ -2,29 +2,30 @@
 include "APIKEY_VERIFY.php";
 $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_aedes");
 
-      if($_SERVER['REQUEST_METHOD'] == "POST"){
-        
-       
-       $id = $_POST['id']; 
-       $sql = "select * from imagem where status = 'F' and id = '$id' ";
-       $result = $mysqli->query($sql);
-       $error = $mysqli->error;
+     if($_SERVER['REQUEST_METHOD'] == "POST"){
+			 
+			 $id = $_POST['id'];
+			 
+			 $sql = "select * from imagem where status = 'F' and id = $id ";
+			 $result = $mysqli->query($sql);
        $resultArray =array();
-       if($result){ 
-        while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-           $imagem_id = $row['id'];
-           $imagemBlob = $row['imageBlob'];
+			 
+			  while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
+            $imagemBlob = $row['imageBlob'];
+            $imagem_id = $row['id'];
+            
+            $resultArray[] = array( 'id' => $imagem_id, 'imageBlob' => $imagemBlob); 
         }
-          $json = array("status" => true, 'id' => $imagem_id,'imageBlob' => $imagemBlob);
-       }else{
-         $json = array("status" => false, "msg" => $error);
-       }
-        
-      }else{
-	      $json = array("status" => false, "msg" => "Request method not accepted!");
-    }
-         
-  mysqli_close($mysqli);
+    
+        $json = array("status" => true, "data" => $resultArray);
+			 
+			 
+		 } 
+		 else{
+			$json = array("status" => false, "msg" => "Request method not accepted!");
+		}	
+
+
 
    header('Access-Control-Allow-Origin: *');
    header('Content-type: application/json');
