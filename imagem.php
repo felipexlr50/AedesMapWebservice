e87bb9faa35bef1daab4b266f1dff006a7ce1918
felipexlr50 <felipexlr50@hotmail.com>
2016-11-25 17:15:07 -0500

    teste

--- imagem.php
+++ imagem.php
@@ -9,16 +9,18 @@ $mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_
 			 $sql = "select * from imagem where status = 'F' and id = $id ";
 			 $result = $mysqli->query($sql);
        $resultArray =array();
+			 $imagemBlob = ""
+			 $imagem_id = 0 
 			 if($result){
 				  while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
             $imagemBlob = $row['imageBlob'];
             $imagem_id = $row['id'];
             
-            $resultArray[] = array( 'id' => $imagem_id, 'imageBlob' => $imagemBlob); 
+           // $resultArray[] = array(); 
         }
     
-        $json = array("status" => true, "data" => $resultArray);
-				 
+       // $json = array("status" => true, "data" => $resultArray);
+				 $json = array("status" => true, 'id' => $imagem_id, 'imageBlob' => $imagemBlob);
 			 }
 			 else{
 					$json = array("status" => false, "msg" => "Algo deu errado :( ");
