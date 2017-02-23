617a87e53742269c2d6b3fe4932fcedbf4071be3
Felipe Machado Baptista <felipexlr50@hotmail.com>
2017-02-23 18:52:58 -0300

    Update pegarPontos.php
    teste

--- pegarPontos.php
+++ pegarPontos.php
@@ -1,6 +1,6 @@
 <?php
 include "APIKEY_VERIFY.php";
-$mysqli = mysqli_connect("localhost", "u517046934_fel", "7c7fd8486","u517046934_aedes");
+$mysqli = mysqli_connect("127.0.0.1", "u517046934_fel", "7c7fd8486","u517046934_aedes");
 
       
        $sql = "select * from imagem where status = 'F' ";
